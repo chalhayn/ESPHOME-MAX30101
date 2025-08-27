3b2619373993d7557c13d895ab3841755002b0d2
chalhayn <41086131+chalhayn@users.noreply.github.com>
2025-08-26 21:23:50 -0500

    Update sensor.py

--- components/max30102_native/sensor.py
+++ components/max30102_native/sensor.py
@@ -1,40 +1,35 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.const import (
-    CONF_ID,
-    STATE_CLASS_MEASUREMENT,
-    UNIT_BEATS_PER_MINUTE,
-    UNIT_PERCENT,
-    ICON_HEART_PULSE,
-    ICON_PERCENT,
-)
+from esphome.components import i2c, sensor
+from esphome.const import CONF_ID
 
-from . import max30102_native_ns, MAX30102NativeComponent
+from . import MAX30102NativeSensor
+
+# Make sure ESPHome loads i2c and sensor first
+DEPENDENCIES = ["i2c"]
+AUTO_LOAD = ["sensor"]
 
 CONF_HEART_RATE = "heart_rate"
 CONF_SPO2 = "spo2"
 
-MAX30102NativeSensor = max30102_native_ns.class_("MAX30102NativeSensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice)
-
 CONFIG_SCHEMA = (
     cv.Schema({
         cv.GenerateID(): cv.declare_id(MAX30102NativeSensor),
+
         cv.Optional(CONF_HEART_RATE): sensor.sensor_schema(
-            unit_of_measurement=UNIT_BEATS_PER_MINUTE,
-            icon=ICON_HEART_PULSE,
+            unit_of_measurement="bpm",
             accuracy_decimals=0,
-            state_class=STATE_CLASS_MEASUREMENT,
+            icon="mdi:heart-pulse",
         ),
+
         cv.Optional(CONF_SPO2): sensor.sensor_schema(
-            unit_of_measurement=UNIT_PERCENT,
-            icon=ICON_PERCENT,
+            unit_of_measurement="%",
             accuracy_decimals=1,
-            state_class=STATE_CLASS_MEASUREMENT,
+            icon="mdi:percent",
         ),
     })
-    .extend(cv.polling_component_schema("1s"))
     .extend(i2c.i2c_device_schema(0x57))
+    .extend(cv.polling_component_schema("1s"))
 )
 
 async def to_code(config):
@@ -43,9 +38,9 @@ async def to_code(config):
     await i2c.register_i2c_device(var, config)
 
     if CONF_HEART_RATE in config:
-        sens = await sensor.new_sensor(config[CONF_HEART_RATE])
-        cg.add(var.set_heart_rate_sensor(sens))
+        hr = await sensor.new_sensor(config[CONF_HEART_RATE])
+        cg.add(var.set_heart_rate_sensor(hr))
 
     if CONF_SPO2 in config:
-        sens = await sensor.new_sensor(config[CONF_SPO2])
-        cg.add(var.set_spo2_sensor(sens))
+        s2 = await sensor.new_sensor(config[CONF_SPO2])
+        cg.add(var.set_spo2_sensor(s2))
