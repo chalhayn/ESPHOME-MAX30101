8106ecc2a8cf7191ee6c2358d8f6b848e7c01207
chalhayn <41086131+chalhayn@users.noreply.github.com>
2025-08-26 21:01:37 -0500

    Update __init__.py

--- components/max30101/__init__.py
+++ components/max30101/__init__.py
@@ -3,16 +3,21 @@ import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['i2c']
+DEPENDENCIES = ["i2c"]
+CODEOWNERS = ["@yourusername"]
 
-max30102_ns = cg.esphome_ns.namespace('max30102')
-MAX30102Component = max30102_ns.class_('MAX30102Component', cg.Component, i2c.I2CDevice)
+max30102_native_ns = cg.esphome_ns.namespace("max30102_native")
+MAX30102NativeComponent = max30102_native_ns.class_("MAX30102NativeComponent", cg.Component, i2c.I2CDevice)
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(MAX30102Component),
-}).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x57))
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(MAX30102NativeComponent),
+    })
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(i2c.i2c_device_schema(0x57))
+)
 
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield i2c.register_i2c_device(var, config)
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
