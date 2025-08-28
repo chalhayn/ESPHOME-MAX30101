df2ae3264a45e01036f1d9705c9c877c411c81b3
chalhayn <41086131+chalhayn@users.noreply.github.com>
2025-08-27 19:06:05 -0500

    Update __init__.py

--- components/max30102_native/__init__.py
+++ components/max30102_native/__init__.py
@@ -1,23 +1,18 @@
+# __init__.py
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
-from esphome.const import CONF_ID
 
 DEPENDENCIES = ["i2c"]
 CODEOWNERS = ["@yourusername"]
 
 max30102_native_ns = cg.esphome_ns.namespace("max30102_native")
-MAX30102NativeComponent = max30102_native_ns.class_("MAX30102NativeComponent", cg.PollingComponent, i2c.I2CDevice)
+MAX30102NativeSensor = max30102_native_ns.class_("MAX30102NativeSensor", cg.PollingComponent, i2c.I2CDevice)
 
 CONFIG_SCHEMA = (
     cv.Schema({
-        cv.GenerateID(): cv.declare_id(MAX30102NativeComponent),
+        cv.GenerateID(): cv.declare_id(MAX30102NativeSensor),
     })
     .extend(cv.COMPONENT_SCHEMA)
     .extend(i2c.i2c_device_schema(0x57))
 )
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
